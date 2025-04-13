a87aafaa1045dc947ce5150d6076da2200896ec2
Trim21 <trim21.me@gmail.com>
2025-04-13 23:37:09 +0800

    add more logging

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mymmrac/telego"
 	"github.com/redis/rueidis"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/samber/lo"
 	"go-simpler.org/env"
 	"golang.org/x/sync/errgroup"
@@ -35,13 +36,20 @@ func init() {
 }
 
 func main() {
+	log.Info().Msg("app start")
+
 	var cfg Config
+	log.Info().Msg("load config")
 	lo.Must0(env.Load(&cfg, nil))
 	cfg.ExternalHttpAddress = strings.TrimSuffix(cfg.ExternalHttpAddress, "/")
 
+	log.Info().Msg("connect to pg")
 	pg := sqlx.MustConnect("postgres", cfg.PgDsn)
+
+	log.Info().Msg("connect to mysql")
 	mysql := sqlx.MustConnect("mysql", cfg.MysqlDsn)
 
+	log.Info().Msg("connect to redis")
 	redisDSN := lo.Must(url.Parse(cfg.RedisDsn))
 	redisPassword, _ := redisDSN.User.Password()
 	redis := lo.Must(rueidis.NewClient(rueidis.ClientOption{
@@ -49,6 +57,7 @@ func main() {
 		InitAddress: []string{redisDSN.Host}},
 	))
 
+	log.Info().Msg("prepare pg table")
 	pg.MustExec(`CREATE TABLE IF NOT EXISTS telegram_notify_chat (
 								chat_id bigint,
 								user_id bigint,
@@ -56,11 +65,13 @@ func main() {
 								primary key (chat_id, user_id)
 			);`)
 
+	log.Info().Msg("create telegram bot")
 	bot := lo.Must(telego.NewBot(cfg.BotToken,
 		telego.WithDefaultLogger(cfg.Debug, true),
 		telego.WithHTTPClient(http.DefaultClient)),
 	)
 
+	log.Info().Msg("get bot info")
 	currentBot := lo.Must(bot.GetMe(context.Background()))
 
 	h := &handler{
@@ -73,8 +84,8 @@ func main() {
 		redis:   redis,
 	}
 
+	log.Info().Msg("start main loop")
 	var eg errgroup.Group
-
 	eg.Go(func() error {
 		return h.processTelegramMessage()
 	})
